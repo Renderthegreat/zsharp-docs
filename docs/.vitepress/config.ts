69bf99cade13f4db032f2f1319f860214e954201
Renderthegreat <renderthegreat.business@gmail.com>
2025-05-19 20:50:06 -0500

    Fix logogit add .!

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,10 +3,10 @@ export default ({
 	description: 'Documentation for the Z# programming language',
 	lang: 'en-US',
 	head: [
-		['link', { rel: 'icon', type: 'image/svg+xml', href: '/uploads/svg/logo.svg' }]
+		['link', { rel: 'icon', type: 'image/svg+xml', href: '/assets/svg/logo.svg' }]
 	],
 	themeConfig: {
-		logo: '/uploads/svg/logo.svg',
+		logo: '/assets/svg/logo.svg',
 		nav: [],
 		sidebar: {}
 	}
