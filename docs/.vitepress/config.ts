122d678fa30742692f2e4f9d5b07bbeebd06d5c9
Renderthegreat <renderthegreat.business@gmail.com>
2025-08-07 20:11:04 -0500

    Theme changes

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,6 @@
-export default ({
+import { defineConfig } from 'vitepress';
+
+export default defineConfig({
 	title: 'Z# Language',
 	description: 'Documentation for the Z# programming language',
 	lang: 'en-US',
@@ -7,8 +9,21 @@ export default ({
 	],
 	themeConfig: {
 		logo: '/assets/svg/logo.svg',
+		siteTitle: 'Z# Language',
 		nav: [],
-		sidebar: {}
+		sidebar: {},
+		footer: {
+			copyright: 'Public Domain',
+			message: 'Z# Language'
+		},
+		socialLinks: [
+			{ icon: 'github', link: 'https://github.com/zsharp/zsharp' },
+			{ icon: 'discord', link: 'https://discord.gg/gGcbaBjtBS' },
+		],
+		search: {
+			provider: 'local',	
+		},
+		
 	},
 	ignoreDeadLinks: true
 });
